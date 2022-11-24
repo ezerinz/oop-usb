d8f8a672a9846ea573f637a3712cd0349368334a
Ezerinz <edwinnnzx@gmail.com>
2022-11-24 19:23:57 +0800

    tugas 3 perhitungan luas

---
+++ Perhitungan-Luas/bangundatar.py
@@ -0,0 +1,39 @@
+# Edwin
+# D0221371
+# Informatika H
+
+# Python harus pakai module untuk memakai abstract class
+# Abstract class adalah class yang tidak bisa dipakai
+# tapi kita bisa membuat class yang "mewarisi" / inherit dari abstract class
+from abc import ABC, abstractmethod
+
+# Untuk mendapatkan pi
+from math import pi
+
+class BangunDatar(ABC):
+    @abstractmethod
+    def luas(self):
+        pass
+
+
+class Lingkaran(BangunDatar):
+    def __init__(self, jari = 0):
+        self.jari = jari
+
+    def luas(self):
+        return pi * (self.jari**2)
+
+class Persegi(BangunDatar):
+    def __init__(self, sisi = 0):
+        self.sisi = sisi
+
+    def luas(self):
+        return self.sisi * self.sisi
+
+class Segitiga(BangunDatar):
+    def __init__(self, alas = 0, tinggi = 0):
+        self.alas = alas
+        self.tinggi = tinggi
+
+    def luas(self):
+        return 0.5 * self.alas * self.tinggi
---
+++ Perhitungan-Luas/main.py
@@ -0,0 +1,37 @@
+# Nama  : Edwin
+# NIM   : D0221371
+# Kelas : Informatika H
+
+from bangundatar import *
+
+lingkaran = Lingkaran()
+segitiga = Segitiga()
+persegi = Persegi()
+luas = 0
+while True:
+    print()
+    print("""1. Hitung Luas Segitiga
+2. Hitung Luas Persegi
+3. Hitung Luas Lingkaran
+4. Berhenti""")
+    pilihan = int(input("=> "))
+
+    if pilihan == 1:
+        segitiga.alas = float(input("Masukkan Alas: "))
+        segitiga.tinggi = float(input("Masukkan Tinggi: "))
+        luas = segitiga.luas()
+    elif pilihan == 2:
+        persegi.sisi = float(input("Masukkan Sisi: "))
+        luas = persegi.luas()
+    elif pilihan == 3:
+        lingkaran.jari = float(input("Masukkan Jari-jari: "))
+        luas = lingkaran.luas()
+    elif pilihan == 4:
+        break
+    else:
+        print("Periksa Kembali Inputan!")
+    
+    print("Luas: ", luas)
+
+print("Selamat Tinggal")
+
